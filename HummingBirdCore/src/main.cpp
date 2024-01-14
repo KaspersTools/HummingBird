5fae00a3b39e679294effd3c8dee4adaf53ef8e7
Kasper de Bruin <bruinkasper@gmail.com>
2024-01-14 02:52:48 +0100

    Cleanup cmake list

--- HummingBirdCore/src/main.cpp
+++ HummingBirdCore/src/main.cpp
@@ -1,11 +1,7 @@
 //windows
-#ifdef _WIN32
-#define SDL_MAIN_HANDLED // avoid link error on Linux Windows Subsystem
-#include <SDL.h>
-#endif
 
-#include "Application.h"
-#include "Log.h"
+#include <Application.h>
+#include <Log.h>
 
 int main(){
   HummingBirdCore::Log::Init();
