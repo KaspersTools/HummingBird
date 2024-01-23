a8cfb4cef456844c2cef37c4d3d3fe4fc34ae244
Kasper de Bruin <bruinkasper@gmail.com>
2024-01-23 16:11:18 +0100

    BROKEN COMMIT -- removed all old submodules before updating to new imgui.

--- vendor/glfw/docs/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['1_0',['1',['../news.html#features_31',1,'New features in version 3.1'],['../news.html#news_31',1,'Release notes for 3.1']]]
-];
--- vendor/glfw/docs/html/search/pages_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['notes_0',['Release notes',['../news.html',1,'']]],
-  ['notitle_1',['notitle',['../index.html',1,'']]]
-];
